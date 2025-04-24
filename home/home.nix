256f2f978735e075ddd9b849945d053e5037cc2b
sh4k0 <carlofedep@gmail.com>
2025-04-24 20:09:16 +0000

    home-manager: added sway config symlink. configuration.nix: installed fonts, packaged nvidia-offload script

--- home/home.nix
+++ home/home.nix
@@ -6,12 +6,14 @@
 
   # targets.genericLinux.enable = true; # enable on non-NixOS
 
-  home.packages = [
-    pkgs.firefox
-    pkgs.fuzzel
-    pkgs.hello
-    pkgs.neovim
-    pkgs.telegram-desktop
+  home.packages = with pkgs; [
+    alacritty
+    firefox
+    fuzzel
+    zellij
+    neovim
+    telegram-desktop
+    yazi
   ];
 
   home.sessionVariables = {
@@ -19,6 +21,7 @@
   };
 
   home.file = {
+    ".config/sway/config".source = dotfiles/sway/config;
   };
 
   home.stateVersion = "24.11";
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,16 +4,6 @@
 
 { inputs, config, lib, pkgs, ... }:
 
-
-  let nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1 &&
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 &&
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia &&
-    export __VK_LAYER_NV_optimus=NVIDIA_only &&
-    exec "$@"
-  '';
-  in
-
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -125,10 +115,19 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    wget
+  #environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
+    (import scripts/nvidia-offload.nix { inherit pkgs; })
+    pkgs.fuzzel
+    pkgs.git
+    pkgs.vim
+    pkgs.wget
+  ];
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    nerd-fonts.jetbrains-mono
   ];
 
   programs.steam = {
