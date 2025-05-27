e81c9b9595fc436e03e0b4912806e0ea884a317c
sh4k0 <carlofedep@gmail.com>
2025-05-27 19:40:08 +0200

    home-manager: ingested swaylock config

--- home/modules/swaylock/default.nix
+++ home/modules/swaylock/default.nix
@@ -4,5 +4,45 @@
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
-  }
+    settings = {
+      ignore-empty-password = true;
+      fade-in = "0.7";
+      clock = true;
+      effect-blur = "10x5";
+      effect-vignette = "0.5:0.5";
+      daemonize = true;
+      indicator = true;
+      screenshots = true;
+      datestr = "%d/%m/%Y";
+      indicator-caps-lock = true;
+      bs-hl-color = "7daea3cc";
+      caps-lock-bs-hl-color = "7daea3cc";
+      caps-lock-key-hl-color = "d3869bcc";
+      font = "JetBrainsMonoNLNerdFontMono";
+      font-size = "32";
+      indicator-idle-visible = true;
+      indicator-radius = "100";
+      indicator-thickness = 7;
+      inside-color = "32302f66";
+      inside-clear-color = "89b48266";
+      inside-caps-lock-color = "e78a4e66";
+      inside-ver-color = "7daea366";
+      inside-wrong-color = "ea696266";
+      key-hl-color = "a9b665cc";
+      layout-bg-color = "32302f00";
+      layout-text-color = "d4be98";
+      line-color = "00000000";
+      ring-color = "e78a4ecc";
+      ring-clear-color = "89b482cc";
+      ring-caps-lock-color = "e78a4ecc";
+      ring-ver-color = "7daea3cc";
+      ring-wrong-color = "ea6962cc";
+      separator-color = "00000000";
+      text-color = "d4be98";
+      text-clear-color = "d4be98";
+      text-caps-lock-color = "d4be98";
+      text-ver-color = "d4be98";
+      text-wrong-color = "d4be98";
+    };
+  };
 }
