18b8bd0712da50881788d8f880f012915312e3ea
sh4k0 <carlofedep@gmail.com>
2025-05-29 17:49:22 +0200

    home-manager: ingested fuzzel config

--- home/modules/fuzzel.nix
+++ home/modules/fuzzel.nix
@@ -6,28 +6,31 @@
     settings = {
       main = {
         font = "JetBrainsMonoNLNerdFontMono:size=10";
-        use-bold = false;
-        prompt = "'🔎 '";
+        use-bold = true;
+        dpi-aware = "yes";
+        prompt = "'🚀 '";
         terminal = "${pkgs.alacritty}/bin/alacritty";
-        horizontal-pad = 12;
-        vertical-pad = 12;
+        lines = 18;
+        width = 45;
+        horizontal-pad = 16;
+        vertical-pad = 16;
         inner-pad = 4;
         image-size-ratio = 1;
         layer = "top";
       };
       border = {
-        width = 2;
+        width = 0;
       };
       colors = {
         background = "282828dd";
         text = "ebdbb2ff";
         prompt = "fe8019ff";
-# placeholder=93a1a1ff
+        #placeholder = "#504945ff";
         input = "fe8019ff";
         match = "fe8019ff";
-        selection = "fe8019ff";
-        selection-text = "282828ff";
-        selection-match = "928374ff";
+        selection = "#504945ff";
+        selection-text = "ebdbb2ff";
+        selection-match = "fe8019ff";
         counter = "93a1a1ff";
         border = "282828dd";
       };
