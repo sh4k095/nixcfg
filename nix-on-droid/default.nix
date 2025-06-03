1eb6f7f7bedfd438e1bf51cd54727a4e493491a3
sh4k0 <carlofedep@gmail.com>
2025-06-03 22:30:22 +0200

    nix-on-droid: defconf

---
+++ nix-on-droid/default.nix
@@ -0,0 +1,53 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.packages = with pkgs; [
+    neovim
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Set your time zone
+  #time.timeZone = "Europe/Berlin";
+
+  # Configure home-manager
+  home-manager = {
+    backupFileExtension = "hm-bak";
+    useGlobalPkgs = true;
+
+    config =
+      { config, lib, pkgs, ... }:
+      {
+        # Read the changelog before changing this value
+        home.stateVersion = "24.05";
+
+        # insert home-manager config
+      };
+  };
+}
