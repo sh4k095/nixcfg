a6bed0e79669e83afd3528171d3e3d847bfe7801
sh4k0 <carlofedep@gmail.com>
2025-07-29 09:56:07 +0200

    home-manager, ssh: removed hosts from ssh config

--- modules/home-manager/ssh/default.nix
+++ modules/home-manager/ssh/default.nix
@@ -5,21 +5,5 @@
     includes = [
       "config.d/ssh_hosts"
     ];
-    #matchBlocks = {
-    #  "gitlab.com" = {
-    #    identityFile = "~/.ssh/gitlab";
-    #  };
-    #  "github.com" = {
-    #    identityFile = "~/.ssh/github";
-    #  };
-    #  "vsc4" = {
-    #    hostname = "vsc4.vsc.ac.at";
-    #    user = "cpaulett";
-    #  };
-    #  "vsc5" = {
-    #    hostname = "vsc5.vsc.ac.at";
-    #    user = "cpaulett";
-    #  };
-    #};
   };
 }
