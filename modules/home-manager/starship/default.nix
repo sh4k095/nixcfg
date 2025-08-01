0757f5a6b958eb70ac71e1e62ca26dd78303cecc
sh4k0 <carlofedep@gmail.com>
2025-08-01 15:36:58 +0200

    added direnv + starship

---
+++ modules/home-manager/starship/default.nix
@@ -0,0 +1,21 @@
+programs.starship = {
+      enable = true;
+      settings = {
+        character = {
+          success_symbol = "[λ](bold green)";
+          error_symbol = "[λ](bold red)";
+        };
+        directory.read_only = " 󰌾";
+        docker_context.symbol = " ";
+        elixir.symbol = " ";
+        git_branch.symbol = " ";
+        git_commit.tag_symbol = "  ";
+        golang.symbol = " ";
+        haskell.symbol = " ";
+        hostname.format = "@[$hostname]($style) ";
+        nix_shell.symbol = " ";
+        python.symbol = " ";
+        rust.symbol = "󱘗 ";
+        username.format = "[$user]($style) ";
+      };
+    };
