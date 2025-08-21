768d0b54c8262b165ae58fbab730e8ba7dd752f3
sh4k0 <carlofedep@gmail.com>
2025-08-21 21:48:36 +0200

    users/sh4k0: refactor

--- users/sh4k0/default.nix
+++ users/sh4k0/default.nix
@@ -1,17 +1,35 @@
 { pkgs, ... }:
 
 {
-  users.users.sh4k0 = {
-    isNormalUser = true;
-    extraGroups = [
-      "adbusers"
-      "audio"
-      "bluetooth"
-      "dialout"
-      "networkmanager"
-      "video"
-      "wheel"
-    ];
-    shell = pkgs.zsh;
+  nix.settings.trusted-users = [ "sh4k0" ];
+  
+  users = {
+    users = {
+      sh4k0 = {
+        shell = pkgs.zsh;
+        uid = 1000;
+        isNormalUser = true;
+        extraGroups = [
+          "adbusers"
+          "audio"
+          "bluetooth"
+          "dialout"
+          "input"
+          "networkmanager"
+          "video"
+          "wheel"
+        ];
+        group = "sh4k0";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2Yr/kP45KafIBopwolJ9bqpIlHUZSbbmIefu6PuVQd sh4k0@erebus"
+        ];
+      };
+    };
+    groups = {
+      sh4k0 = {
+        gid = 1000;
+      };
+    };
   };
+  programs.zsh.enable = true;
 }
