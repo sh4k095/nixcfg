d236d2598711298b513edf07a086bbfedc911308
sh4k0 <carlofedep@gmail.com>
2025-08-24 18:07:37 +0200

    hosts/cetus: enabled AdGuardHome admin interface

--- hosts/cetus/default.nix
+++ hosts/cetus/default.nix
@@ -74,6 +74,11 @@
     };
     adguardhome = {
       enable = true;
+      settings = {
+        http = {
+          address = "127.0.0.1:3000";
+        };
+      };
     };
     automatic-timezoned = {
       enable = true;
