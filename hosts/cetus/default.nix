ce9146233a31d6d046bd2ee2287882647d8fb5ae
sh4k0 <carlofedep@gmail.com>
2025-10-26 09:56:05 +0100

    cetus: debug AdGuard DNS

--- hosts/cetus/default.nix
+++ hosts/cetus/default.nix
@@ -84,7 +84,7 @@
     };
     adguardhome = {
       enable = true;
-    #  openFirewall = true;
+      openFirewall = true;
     #  settings = {
     #    http = {
     #      address = "127.0.0.1:3000";
