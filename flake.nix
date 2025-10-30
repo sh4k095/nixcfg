9ded3f479c08a1d7d00d179908fed1d00f345492
sh4k0 <carlofedep@gmail.com>
2025-10-30 19:54:00 +0100

    flake.nix: reformat, experimenting with flake-parts

--- flake.nix
+++ flake.nix
@@ -28,85 +28,96 @@
       url = "github:nix-community/nix-on-droid";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
     tuxedo-nixos = {
       url = "github:sund3RRR/tuxedo-nixos";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-wayland,
-    home-manager,
-    sops-nix,
-    disko,
-    nixvim,
-    nix-on-droid,
-    tuxedo-nixos,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    nixosConfigurations = {
-      # erebus
-      erebus = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/erebus
-          sops-nix.nixosModules.sops
-          tuxedo-nixos.nixosModules.default
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.sh4k0 = ./hosts/erebus/modules/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs outputs;};
-          }
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-wayland,
+      home-manager,
+      sops-nix,
+      disko,
+      nixvim,
+      nix-on-droid,
+      flake-parts,
+      tuxedo-nixos,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+    in
+    {
+      nixosConfigurations = {
+        # erebus
+        erebus = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/erebus
+            sops-nix.nixosModules.sops
+            tuxedo-nixos.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.sh4k0 = ./hosts/erebus/modules/home.nix;
+              home-manager.extraSpecialArgs = { inherit inputs outputs; };
+            }
+          ];
+        };
+        # cetus
+        cetus = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/cetus
+            sops-nix.nixosModules.sops
+            disko.nixosModules.disko
+          ];
+        };
+        # installer
+        installer = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          system = "x86_64-linux";
+          modules = [
+            (
+              { pkgs, modulesPath, ... }:
+              {
+                imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+              }
+            )
+            ./hosts/installer
+          ];
+        };
       };
-      # cetus
-      cetus = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/cetus
-          sops-nix.nixosModules.sops
-          disko.nixosModules.disko
-        ];
+      homeConfigurations = {
+        # Steam Deck
+        "sh4k0@steamdeck" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./home
+            nixvim.homeModules.nixvim
+            sops-nix.homeManagerModules.sops
+          ];
+        };
       };
-      # installer
-      installer = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        system = "x86_64-linux";
-        modules = [
-          ({ pkgs, modulesPath, ... }: {
-            imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-          })
-          ./hosts/installer
-        ];
+      # Pixel 9
+      nixOnDroidConfigurations = {
+        "sh4k0@pixel9" = nix-on-droid.lib.nixOnDroidConfiguration {
+          pkgs = import nixpkgs { system = "aarch64-linux"; };
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/ceto
+          ];
+        };
       };
     };
-    homeConfigurations = {
-      # Steam Deck
-      "sh4k0@steamdeck" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {system = "x86_64-linux";};
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home
-          nixvim.homeModules.nixvim
-          sops-nix.homeManagerModules.sops
-        ];
-      };
-    };
-    # Pixel 9
-    nixOnDroidConfigurations = {
-      "sh4k0@pixel9" = nix-on-droid.lib.nixOnDroidConfiguration {
-        pkgs = import nixpkgs {system = "aarch64-linux";};
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/ceto
-        ];
-      };
-    };
-  };
 }
