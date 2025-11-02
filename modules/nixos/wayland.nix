ec9bf94a9b008b859cc7bcbbcbf75d623c170b4e
sh4k0 <carlofedep@gmail.com>
2025-11-02 15:48:58 +0100

    learning how to split flake-parts modules

---
+++ modules/nixos/wayland.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }:
+
+{
+  flake.modules.nixos.wayland = {
+    nix.settings = {
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nixpkgs-wayland.cachix.org"
+      ];
+    };
+    nixpkgs.overlays = [
+      inputs.nixpkgs-wayland.overlay
+    ];
+  };
+}
