8db1135a6801b97f7fa15beeee28f35f444fbab4
sh4k0 <carlofedep@gmail.com>
2025-12-01 16:32:49 +0100

    flake.lock: flake update

---
+++ modules/hosts/erebus/nvidia.nix
@@ -0,0 +1,38 @@
+{
+  flake.modules.nixos.host_erebus = { config, lib, pkgs, ... }: {
+
+    services.xserver.videoDrivers = [
+      "amd"
+      "nvidia"
+    ];
+    
+    hardware = {
+      graphics = {
+        enable = true;
+      };
+      nvidia = {
+        open = true;
+        modesetting.enable = true;
+        powerManagement = {
+          enable = true;
+          finegrained = true;
+        };
+        prime = {
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
+          amdgpuBusId = "PCI:1:0:0";
+          nvidiaBusId = "PCI:6:0:0";
+        };
+        dynamicBoost.enable = true;
+      };
+    };
+    services.udev = {
+      extraRules = ''
+        SYMLINK=="dri/by-path/pci-0000:01:00.0-card", SYMLINK+="dri/igpu1"
+        SYMLINK=="dri/by-path/pci-0000:06:00.0-card", SYMLINK+="dri/dgpu1"
+      '';
+    };
+  };
+}
