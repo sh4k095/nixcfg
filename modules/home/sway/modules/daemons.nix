a2ac414556d13da725c7d2500152992f08bb0e06
sh4k0 <carlofedep@gmail.com>
2025-12-16 13:07:51 +0100

    sway: use lib.getExe for executables in daemons declarations

--- modules/home/sway/modules/daemons.nix
+++ modules/home/sway/modules/daemons.nix
@@ -1,9 +1,9 @@
 {
-  flake.modules.homeManager.sway = { config, pkgs, ... }: {
+  flake.modules.homeManager.sway = { config, lib, pkgs, ... }: {
     config.wayland.windowManager.sway.config.startup = [
-      { command = "${pkgs.autotiling-rs}/bin/autotiling-rs"; always = true; }
+      { command = lib.getExe pkgs.autotiling-rs; always = true; }
       { command = ''
-        ${pkgs.swayidle}/bin/swayidle -w \
+        ${lib.getExe pkgs.swayidle} -w \
         timeout 300 "swaylock -f -c 000000" \
         timeout 600 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
         before-sleep  'playerctl pause' \
